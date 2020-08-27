b8df43f77fcc2e484585a51854c1ba4c70b61f67
Anthony Weems <amlweems@gmail.com>
2020-08-27 09:45:25 -0500

    update result struct to include additional fields

--- functions/events/events.go
+++ functions/events/events.go
@@ -1,6 +1,13 @@
 package events
 
+import (
+	"time"
+)
+
 type AuthRequest struct {
+	// CampaignID is used to track the results of the task
+	CampaignID uint `json:"campaign_id"`
+
 	// Username is the username at the identity provider
 	Username string `json:"username"`
 
@@ -15,13 +22,31 @@ type AuthRequest struct {
 }
 
 type AuthResponse struct {
+	// CampaignID is used to track the results of the task
+	CampaignID uint `json:"campaign_id"`
+
+	// IP is the originating IP of the credential guess
+	IP string `json:"ip"`
+
+	// Timestamp is the time that we made the request
+	Timestamp time.Time `json:"timestamp"`
+
+	// Username is the username at the identity provider
+	Username string `json:"username"`
+
+	// Password is the password to guess against the identity provider
+	Password string `json:"password"`
+
 	// Valid indicates the provided credential was valid
 	Valid bool `json:"valid"`
 
 	// Locked will be true iff the account is known to be locked
 	Locked bool `json:"locked"`
 
-	// RateLimit indicates the provider has detected a large number of requests
+	// MFA will be true iff the account requires MFA to log in
+	MFA bool `json:"mfa"`
+
+	// RateLimited indicates the provider has detected a large number of requests
 	RateLimited bool `json:"rate_limited"`
 
 	// Additional metadata from the auth provider (e.g. information about MFA)
