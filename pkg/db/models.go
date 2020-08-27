7c99ec406b8d08be590c4592581991884a3e786c
Anthony Weems <amlweems@gmail.com>
2020-08-27 09:47:00 -0500

    db: add result model

--- pkg/db/models.go
+++ pkg/db/models.go
@@ -37,6 +37,42 @@ type Campaign struct {
 	ProviderMetadata postgres.Jsonb
 }
 
+type Result struct {
+	gorm.Model
+
+	// TODO: will this link properly in gorm to the campaign object?
+	// CampaignID is used to track the results of the task
+	CampaignID uint `json:"campaign_id"`
+
+	// IP is the originating IP of the credential guess
+	IP string `json:"ip"`
+
+	// Timestamp is the time that we made the request
+	Timestamp time.Time `json:"timestamp"`
+
+	// Username is the username at the identity provider
+	Username string `json:"username"`
+
+	// Password is the password to guess against the identity provider
+	Password string `json:"password"`
+
+	// Valid indicates the provided credential was valid
+	Valid bool `json:"valid"`
+
+	// Locked will be true iff the account is known to be locked
+	Locked bool `json:"locked"`
+
+	// MFA will be true iff the account requires MFA to log in
+	MFA bool `json:"mfa"`
+
+	// RateLimited indicates the provider has detected a large number of requests
+	RateLimited bool `json:"rate_limited"`
+
+	// TODO: does this need to be postgres.Jsonb?
+	// Additional metadata from the auth provider (e.g. information about MFA)
+	Metadata map[string]interface{} `json:"metadata"`
+}
+
 type Task struct {
 	// NotBefore will prevent execution until this time
 	NotBefore time.Time `json:"not_before"`
