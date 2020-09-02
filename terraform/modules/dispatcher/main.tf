6ee305195bdf6b708e0c8666e9831c61f69afc49
Anthony Weems <amlweems@gmail.com>
2020-09-02 16:51:18 -0500

    terraform: initial dispatcher module

--- terraform/modules/dispatcher/main.tf
+++ terraform/modules/dispatcher/main.tf
@@ -1,4 +1,113 @@
 
-# dispatcher k8s deployment
-# dispatcher GSA and KSA
-# pubsub subscription
+locals {
+  name = "dispatcher"
+ 
+  labels = {
+    app = "dispatcher"
+  }
+ 
+  env = {
+    "PROJECT_ID"      = var.project,
+    "RESULT_TOPIC_ID" = var.pubsub_topic,
+    "SUBSCRIPTION_ID" = var.pubsub_subscription,
+    "WORKER_NAME"     = var.worker_name,
+  }
+
+  worker_config = jsonencode({
+    "url"   = var.worker_url,
+    "token" = var.worker_token,
+  })
+}
+
+resource "google_service_account" "server" {
+  project      = var.project
+  account_id   = "trident-${local.name}-sa"
+  display_name = "Trident ${local.name}"
+}
+
+resource "google_service_account_iam_member" "workload-identity" {
+  service_account_id = google_service_account.server.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${var.project}.svc.id.goog[${var.namespace}/${local.name}]"
+}
+
+resource "google_pubsub_topic_iam_member" "server-topic" {
+  topic  = var.pubsub_topic
+  role   = "roles/pubsub.publisher"
+  member = "serviceAccount:${google_service_account.server.id}"
+}
+
+resource "google_pubsub_subscription_iam_member" "server-subcription" {
+  subscription = var.pubsub_subscription
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:${google_service_account.server.id}"
+}
+
+resource "kubernetes_service_account" "server" {
+  metadata {
+    namespace   = var.namespace
+    name        = local.name
+    annotations = {
+      "iam.gke.io/gcp-service-account" = google_service_account.server.id
+    }
+  }
+}
+
+resource "kubernetes_config_map" "worker" {
+  metadata {
+    namespace = var.namespace
+    name      = "dispatcher-worker"
+  }
+
+  data = {
+    config = local.worker_config
+  }
+}
+
+resource "kubernetes_deployment" "server" {
+  metadata {
+    namespace = var.namespace
+    name      = local.name
+    labels    = local.labels
+  }
+
+  spec {
+    replicas = 20
+
+    selector {
+      match_labels = local.labels
+    }
+
+    template {
+      metadata {
+        labels = local.labels
+      }
+
+      spec {
+        service_account_name = kubernetes_service_account.server.metadata[0].name
+        container {
+          image = "${var.image}:${var.image_version}"
+          name  = local.name
+
+          dynamic "env" {
+            for_each = local.env
+            content {
+              name  = env.key
+              value = env.value
+            }
+          }
+
+          env {
+            name = "WORKER_CONFIG"
+            value_from {
+              config_map_key_ref {
+                name = kubernetes_config_map.worker.metadata[0].name
+                key  = "config"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ terraform/modules/dispatcher/variables.tf
@@ -0,0 +1,58 @@
+# -----------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These parameters must be supplied when consuming this module.
+# -----------------------------------------------------------------------------
+
+variable "project" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "pubsub_topic" {
+  description = "The name of the PubSub topic to publish results to"
+  type        = string
+}
+
+variable "pubsub_subscription" {
+  description = "The name of the PubSub subscription to fetch jobs from"
+  type        = string
+}
+
+variable "worker_url" {
+  description = "The URL to a webhook worker for job submission"
+  type        = string
+}
+
+variable "worker_token" {
+  description = "The access token to use when authenticating to a worker"
+  type        = string
+}
+
+# -----------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# Generally, these values won't need to be changed.
+# -----------------------------------------------------------------------------
+
+variable "namespace" {
+  description = "The Kubernetes namespace for deployment"
+  type        = string
+  default     = "trident"
+}
+
+variable "image" {
+  description = "The container image to deploy"
+  type        = string
+  default     = "gcr.io/praetorian-red-team-public/dispatcher"
+}
+
+variable "image_version" {
+  description = "The container image version"
+  type        = string
+  default     = "latest"
+}
+
+variable "worker_name" {
+  description = "The type of worker to submit jobs to"
+  type        = string
+  default     = "webhook"
+}
