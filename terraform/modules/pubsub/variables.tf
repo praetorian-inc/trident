88e9b44bf89b1ab9a649bb147b4417633fe9642a
Anthony Weems <amlweems@gmail.com>
2020-09-02 17:06:00 -0500

    terraform: initial pubsub module

---
+++ terraform/modules/pubsub/variables.tf
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These parameters must be supplied when consuming this module.
+# -----------------------------------------------------------------------------
+
+variable "project" {
+  description = "The GCP project ID"
+  type        = string
+}
