001402e07bcc72ef02d68a340ac3d932a8e80e9f
Anthony Weems <amlweems@gmail.com>
2020-09-09 08:42:59 -0500

    client: go fmt

--- pkg/commands/client/results.go
+++ pkg/commands/client/results.go
@@ -41,13 +41,13 @@ var resultsCmd = &cobra.Command{
 
 func init() {
 	resultsCmd.Flags().StringVarP(&flagReturnedFields, "return", "r", "*",
-        "the list of fields you would like to see from the results (comma-separated string)")
+		"the list of fields you would like to see from the results (comma-separated string)")
 
 	resultsCmd.Flags().StringVarP(&flagFilter, "filter", "f", `{"valid":true}`,
-        "filter on db results (specified in JSON)")
+		"filter on db results (specified in JSON)")
 
 	resultsCmd.Flags().StringVarP(&flagOutputFormat, "output-format", "o", "table",
-        "output format (table, csv, json)")
+		"output format (table, csv, json)")
 	rootCmd.AddCommand(resultsCmd)
 }
 
