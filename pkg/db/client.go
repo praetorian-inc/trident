9401a849d617185205251700b3257f36b00d70a2
Robert Leonard <robert.leonard@praetorian.com>
2020-10-01 16:55:34 -0400

    changed query for list

--- pkg/db/client.go
+++ pkg/db/client.go
@@ -231,7 +231,7 @@ func (t *TridentDB) StreamingInsertResults() chan *Result {
 func (t *TridentDB) ListCampaign() ([]Campaign, error) {
 	var campaigns []Campaign
 
-	err := t.db.Find(&campaigns).
+	err := t.db.Select('campaign_id').Find(&campaigns).
 		Error
 	if err != nil {
 		return nil, err
@@ -243,14 +243,12 @@ func (t *TridentDB) ListCampaign() ([]Campaign, error) {
 func (t *TridentDB) DescribeCampaign(query Query) (Campaign, error) {
 	var result Campaign
 
-	// err := t.db.Select(query.ReturnedFields).
-	// 	Where(query.Filter).
-	// 	Order("timestamp DESC").
-	// 	Find(&result).
-	// 	Error
-	// if err != nil {
-	// 	return result, err
-	// }
+	err := t.db.Where(query.Filter).
+		Find(&result).
+		Error
+	if err != nil {
+		return result, err
+	}
 
 	return result, nil
 }
